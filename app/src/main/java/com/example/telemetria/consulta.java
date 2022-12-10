ec48c8c9d6eed1585320e1e0c82f846ec9882788
JeffersonCabello <jefcsanc@espol.edu.ec>
2022-12-10 15:22:37 -0500

    interfaz consulta

--- app/src/main/java/com/example/telemetria/consulta.java
+++ app/src/main/java/com/example/telemetria/consulta.java
@@ -3,12 +3,39 @@ package com.example.telemetria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class consulta extends AppCompatActivity {
+
+    private TextView tv1;
+    private ListView lv1;
+
+    private String extintores [] = {};
+    private String marca [] = {};
+    private String ubicacion [] = {};
+    private String porcentaje [] = {};
+    private String expiracion [] = {};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulta);
+
+        tv1 = (TextView)findViewById(R.id.tv1);
+        lv1 = (ListView)findViewById(R.id.lv1);
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, extintores);
+        lv1.setAdapter(adapter);
+
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
     }
 }
